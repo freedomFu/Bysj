2927ea96fd143dcdb492c9bc48af418acab44480
freedomFu <174780597@qq.com>
2019-05-16 22:01:43 +0800

    第一期界面完成

--- src/com/folm/bysj/gui/FormPanel.java
+++ src/com/folm/bysj/gui/FormPanel.java
@@ -1,5 +1,7 @@
 package com.folm.bysj.gui;
 
+import com.folm.bysj.model.GroupManager;
+import com.folm.bysj.model.GroupMember;
 import com.folm.bysj.model.GroupSinature;
 
 import javax.swing.*;
@@ -7,14 +9,21 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.List;
 
 public class FormPanel extends JPanel{
 
     private JLabel nameLabel;
     private JTextField nameField;
     private JButton okBtn;
+    private JButton checkBtn;
+    private JButton openSign;
     private GroupSinature gp;
     private InfoFrame in;
+    private List<GroupMember> lists;
+    private int x;
 
     public FormPanel(GroupSinature gp, InfoFrame in){
         this.gp = gp;
@@ -22,11 +31,16 @@ public class FormPanel extends JPanel{
         Dimension dim = getPreferredSize();
         dim.width = 250;
         setPreferredSize(dim);
+        lists = gp.getMemberList();
 
         nameLabel = new JLabel("Msg: ");
         nameField = new JTextField(10);
 
-        okBtn = new JButton("OK");
+        okBtn = new JButton("Sign");
+        checkBtn = new JButton("Check");
+        openSign = new JButton("Open");
+        x = (int)((new Random().nextDouble())*65537.0);
+
 
         // 添加到界面上 使用工具
         Border innerBorder = BorderFactory.createTitledBorder("Msg Signature");
@@ -38,10 +52,78 @@ public class FormPanel extends JPanel{
         okBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println(x);
                 String s = nameField.getText();
                 if(null != s && !s.equals("")){
-                    TextPanel tp = in.getTextPanel();
-                    tp.appendTextLn(s);
+                    int size = lists.size();
+                    if(size>0){
+                        x = x%size;
+                        TextPanel tp = in.getTextPanel();
+                        // 随机取出一个对象进行签名：
+                        GroupMember gm = lists.get(x);
+                        Object[] res = gm.sign(gp, s);
+                        tp.appendTextLn("群成员签名成功！（隐藏的x是："+x+"）");
+                        tp.appendTextLn((String)res[0]);
+                        tp.appendTextLn("产生的si："+res[1]);
+                        tp.appendTextLn("存储的pi："+res[2]);
+                    }else{
+                        JOptionPane.showMessageDialog(null, "请先添加群成员", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null, "请输入消息", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        checkBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(x);
+                String s = nameField.getText();
+                if(null != s && !s.equals("")){
+                    int size = lists.size();
+                    if(size>0){
+                        x = x%size;
+                        TextPanel tp = in.getTextPanel();
+                        // 群管理员
+                        GroupManager gman = new GroupManager();
+                        GroupMember gm = lists.get(x);
+                        boolean flag = gman.isSignCorrect(gp, gm, s);
+                        if(flag){
+                            tp.appendTextLn("验证签名成功！");
+                        }else{
+                            tp.appendTextLn("验证签名失败！");
+                        }
+                    }else{
+                        JOptionPane.showMessageDialog(null, "请先添加群成员", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null, "请输入消息", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        openSign.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(x);
+                String s = nameField.getText();
+                if(null != s && !s.equals("")){
+                    int size = lists.size();
+                    if(size>0){
+                        x = x%size;
+                        TextPanel tp = in.getTextPanel();
+                        // 群管理员
+                        GroupManager gman = new GroupManager();
+                        GroupMember gm = lists.get(x);
+                        long id = gman.openSign(gp, gm, s);
+                        tp.appendTextLn("打开签名成功！");
+                        tp.appendTextLn("加密的成员id是："+id);
+                    }else{
+                        JOptionPane.showMessageDialog(null, "请先添加群成员", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null, "请输入消息", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -76,8 +158,28 @@ public class FormPanel extends JPanel{
         gc.weighty = 2.0;
 
         gc.gridx = 1;
-        gc.insets = new Insets(0, 0, 0, 0);
+        gc.insets = new Insets(10, 0, 0, 0);
         gc.anchor = GridBagConstraints.FIRST_LINE_START;
         add(okBtn, gc);
+
+        /******************* Next  Row ****************/
+        gc.gridy++;
+        gc.weightx = 1;
+        gc.weighty = 2.2;
+
+        gc.gridx = 1;
+        gc.insets = new Insets(0, 0, 0, 0);
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        add(checkBtn, gc);
+
+        /******************* Next  Row ****************/
+        gc.gridy++;
+        gc.weightx = 1;
+        gc.weighty = 2.4;
+
+        gc.gridx = 1;
+        gc.insets = new Insets(0, 0, 0, 0);
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        add(openSign, gc);
     }
 }
